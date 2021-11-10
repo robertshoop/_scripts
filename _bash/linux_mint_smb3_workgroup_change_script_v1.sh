200548ea18bae1da251055965e4516f9f74d136b
Robert Shoop <robertshoop@gmail.com>
2021-11-09 22:36:55 -0800

    adding bash dir

---
+++ _bash/linux_mint_smb3_workgroup_change_script_v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /etc/samba/smb.conf
+while IFS= read -r line
+do
+
+# find [global] and add max client under it
+if [ "$line" != "client max protocol = SMB3" ]; then
+     if [ "$line" = "[global]" ]; then
+     	echo "[global]" >> other.txt
+	echo "client max protocol = SMB3" >> other.txt
+     elif [ "$line" = "   workgroup = WORKGROUP" ]; then
+     	echo "workgroup = TRAILERPARK" >> other.txt
+     else
+	echo "$line" >> other.txt
+     fi
+fi
+
+# end of loop
+done < smb.conf
+
+
+#cleanup temporary files
+rm smb.conf
+mv other.txt smb.conf
