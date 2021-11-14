868677a11e2d6566d8d272098fc281e1403e8cfa
Robert Shoop <robertshoop@gmail.com>
2021-11-14 03:36:12 -0800

    Modifing vmWare Script

--- _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
+++ _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
@@ -1,16 +1,13 @@
 #Do not use .OVA files as they currently do not work with this method, extract the .OVA files using 7-Zip and then point the cmdlets to the .OVF file that is extracted:
 
-#Variables are below for refernece:
-#
 # example path "C:\_vmware\win11-client-01\win11-client-01.ovf" without quotes.
 Write-Host "`n"
 $sourceFile = Read-Host -Prompt 'Input the path to the .OVF file you wish to upload to your vSphere / ESXi server.'
-#
-# example "buzzesx01" without quotes.
+
+# Use hostname or FQDN for the vSphere / ESXi host:
 Write-Host "`n"
 $esxServerName = Read-Host -Prompt 'Input the name of your vSphere / ESXi server.'
-#
-# exmaple "win11-client-03" without quotes.
+
 Write-Host "`n"
 $vmNewName = Read-Host -Prompt 'Name the VM you wish to upload.'
 Write-Host "`n"
@@ -20,10 +17,9 @@ Write-Host "Connecting to vSphere / ESXi host." -foregroundcolor green -backgrou
 
 Connect-VIServer -Server $esxServerName
 
-# Import the vApp with the varibles above
+# Imports the vApp with the varibles above
 Import-vApp -Source $sourceFile -VMHost $esxServerName -Name $vmNewName
 
-# Disconnect from the vSphere Server
 Disconnect-VIServer
 
 Write-Host "Your VM has been successfully uploaded!" -foregroundcolor green -backgroundcolor black
