2fb09ddf35f05ee6592853a3e289f9ba246e1fe2
Robert Shoop <robertshoop@gmail.com>
2021-11-13 20:01:26 -0800

    import_vm PoSh script added

--- _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
+++ _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
@@ -3,15 +3,19 @@
 #Variables are below for refernece:
 #
 # example path "C:\_vmware\win11-client-01\win11-client-01.ovf" without quotes.
-
+Write-Host "`n"
 $sourceFile = Read-Host -Prompt 'Input the path to the .OVF file you wish to upload to your vSphere / ESXi server.'
 #
 # example "buzzesx01" without quotes.
-$esxServerName = Read-Host -Prompt 'Insert the name of your vSphere / ESXi server.'
+Write-Host "`n"
+$esxServerName = Read-Host -Prompt 'Input the name of your vSphere / ESXi server.'
 #
 # exmaple "win11-client-03" without quotes.
-$vmNewName = Read-Host -Prompt 'Insert the name of your vSphere / ESXi server.'
-
+Write-Host "`n"
+$vmNewName = Read-Host -Prompt 'Name the VM you wish to upload.'
+Write-Host "`n"
+Write-Host "******* You will be prompted for credentials. *******" -foregroundcolor yellow -backgroundcolor black
+Write-Host "`n"
 Write-Host "Connecting to vSphere / ESXi host." -foregroundcolor green -backgroundcolor black
 
 Connect-VIServer -Server $esxServerName
@@ -22,4 +26,4 @@ Import-vApp -Source $sourceFile -VMHost $esxServerName -Name $vmNewName
 # Disconnect from the vSphere Server
 Disconnect-VIServer
 
-Write-Host "VM has been successfully uploaded!" -foregroundcolor green -backgroundcolor black
+Write-Host "Your VM has been successfully uploaded!" -foregroundcolor green -backgroundcolor black
