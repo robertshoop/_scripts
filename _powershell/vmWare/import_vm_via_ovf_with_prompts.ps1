6b401870f97092602c3e7c4f8d7e2b47fbd91416
Robert Shoop <robertshoop@gmail.com>
2021-11-14 23:50:17 -0800

    More vmWare stuff

--- _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
+++ _powershell/vmWare/import_vm_via_ovf_with_prompts.ps1
@@ -2,7 +2,30 @@
 
 # example path "C:\_vmware\win11-client-01\win11-client-01.ovf" without quotes.
 Write-Host "`n"
-$sourceFile = Read-Host -Prompt 'Input the path to the .OVF file you wish to upload to your vSphere / ESXi server.'
+Write-Host "This script allows a VM to be imported into vmWare." -foregroundcolor Green -backgroundcolor Black
+Write-Host "`n"
+$confirmation = Read-Host "Press Y to proceed or any other key to exit"
+
+if ($confirmation -eq 'y') {
+    # proceed
+    Write-Host "`n"
+    Write-Host "================" -foregroundcolor Green -backgroundcolor Black
+    Write-Host "Running Script." -foregroundcolor Green -backgroundcolor Black
+    Write-Host "================" -foregroundcolor Green -backgroundcolor Black
+}
+if ($confirmation -ne 'y') {
+    Write-Host "`n"
+    Write-Host "================" -foregroundcolor Red -backgroundcolor Black
+    Write-Host "Stopping Script." -foregroundcolor Red -backgroundcolor Black
+    Write-Host "================" -foregroundcolor Red -backgroundcolor Black
+    Write-Host "`n"
+
+    Break Script
+
+}
+
+Write-Host "`n"
+$sourceFile = Read-Host -Prompt 'Input the path to the .OVF file you wish to upload.'
 
 # Use hostname or FQDN for the vSphere / ESXi host:
 Write-Host "`n"
@@ -11,7 +34,9 @@ $esxServerName = Read-Host -Prompt 'Input the name of your vSphere / ESXi server
 Write-Host "`n"
 $vmNewName = Read-Host -Prompt 'Name the VM you wish to upload.'
 Write-Host "`n"
-Write-Host "******* You will be prompted for credentials. *******" -foregroundcolor yellow -backgroundcolor black
+Write-Host "==========================================================" -foregroundcolor Yellow -backgroundcolor Black
+Write-Host "         The script will now ask for credentials.         " -foregroundcolor Yellow -backgroundcolor Black
+Write-Host "==========================================================" -foregroundcolor Yellow -backgroundcolor Black
 Write-Host "`n"
 Write-Host "Connecting to vSphere / ESXi host." -foregroundcolor green -backgroundcolor black
 
@@ -20,7 +45,28 @@ Connect-VIServer -Server $esxServerName
 # Imports the vApp with the varibles above
 Import-vApp -Source $sourceFile -VMHost $esxServerName -Name $vmNewName
 Write-Host "`n"
-Write-Host "Your VM has been successfully uploaded!" -foregroundcolor green -backgroundcolor black
+Write-Host "$vmNewName has been successfully uploaded!" -foregroundcolor green -backgroundcolor black
 Write-Host "`n"
-Write-Host "Do you wish to disconnect your vSphere session?"
-Disconnect-VIServer
+
+$turnOnVM = Read-Host "Would you like to power on the VM?"
+
+if ($turnOnVM -eq 'y') {
+    # proceed
+    Write-Host "`n"
+    Write-Host "============================================" -foregroundcolor green -backgroundcolor black
+    Write-Host "$vmNewName is powering on." -foregroundcolor Green -backgroundcolor Black
+    Write-Host "============================================" -foregroundcolor green -backgroundcolor black
+    Write-Host "`n"
+
+    Start-VM -VM $vmNewName -Confirm:$false
+}
+
+if ($turnOnVM -ne 'y') {
+    Write-Host "`n"
+    Write-Host "=============================================" -foregroundcolor Yellow -backgroundcolor Black
+    Write-Host "$vmNewName will stay off." -foregroundcolor Yellow -backgroundcolor Black
+    Write-Host "=============================================" -foregroundcolor Yellow -backgroundcolor Black
+    Write-Host "`n"
+
+    Break Script
+}
