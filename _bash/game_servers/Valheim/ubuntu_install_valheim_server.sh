5722985821dc2e9d9332afdfb2167437bc5c822b
Robert Shoop <robertshoop@gmail.com>
2021-11-16 23:46:49 -0800

    valheim ded server auto config bash

--- _bash/game_servers/Valheim/ubuntu_install_valheim_server.sh
+++ _bash/game_servers/Valheim/ubuntu_install_valheim_server.sh
@@ -1,3 +1,5 @@
+# WORK IN PROGRESS
+#!/bin/bash
 # Using Valve's documentation from https://developer.valvesoftware.com/wiki/SteamCMD to create this script
 # Tested script on Ubuntu 21.10 with latest updates as of 11-16-2021
 #
@@ -18,8 +20,44 @@ sudo apt install lib32gcc-s1 -y
 # Install SteamCMD
 sudo apt install steamcmd -y
 
-
 # Run SteamCMD and install Valheim dedicated server to /usr/games/valheim
-/usr/games/steamcmd +force_install_dir /usr/games/valheim +login anonymous +app_update 896660 validate +quit
+sudo /usr/games/steamcmd +force_install_dir /usr/games/valheim +login anonymous +app_update 896660 validate +quit
 
-# Change server name, password, and 
+# Backup the /usr/games/valheim/start_server.sh file before making changes to it
+sudo cp /usr/games/valheim/start_server.sh /usr/games/valheim/start_server_backup.sh
+
+# Asks the user on what they would like to change the server name, world name, password, and port to:
+echo
+echo "Please provide the following information for the Valheim Dedicated Server:"
+echo "==========================================================================="
+echo
+echo "What would you like the name the Valheim server?:"
+read valServerName
+echo
+echo "What TCP/IP port do you wish to run the Valheim server on? (default is 2456):"
+read valServerPort
+echo
+echo "What would you like the name the Valheim world instance?"
+read valWorldName
+echo
+echo "What would you like the password to be for the Valheim server"
+echo "NOTE: The password needs to be at least 5 characters and the password can't be in the server name!"
+read valServerPassword
+echo
+echo "The following changes have been made to the Valheim Dedicated Server:"
+echo
+echo "The Server name has been changed to:" $valServerName
+echo "The Server port is set to:" $valServerPort
+echo "The World name is set to:" $valWorldName
+echo "The password is set to:" $valServerPassword
+echo
+echo "Run /usr/games/valheim/start_server.sh to start your Valheim Dedicated Server!"
+echo
+
+# Use sed to change the properties of the /usr/games/valheim/start_server.sh file
+sed -i "s/My server/$valServerName/g" "/usr/games/valheim/start_server.sh"
+sed -i "s/2456/$valServerPort/g" "/usr/games/valheim/start_server.sh"
+sed -i "s/Dedicated/$valWorldName/g" "/usr/games/valheim/start_server.sh"
+sed -i "s/secret/$valServerPassword/g" "/usr/games/valheim/start_server.sh"
+
+# Starts the dedicated server
