2aba5aae9f549659214295c4ed2ea2ffa93a4dd6
Robert Shoop <robertshoop@gmail.com>
2021-11-21 14:31:54 -0800

    Adding _ansible directory to _scripts branch

---
+++ _bash/app_servers/jenkins-ci/jenkins_password_reset_script.sh
@@ -0,0 +1,10 @@
+Click on People on the left-hand navigation menu.
+Click on the Admin.
+Delete the user account.
+Navigate to Jenkins / Manage Jenkins.
+Click on Configure Global Security
+Check the Enable Security check box
+Under Security Realm, select Jenkins’ own user database
+In the Authorization section, select Logged-in users can do anything.
+Unselect Allow anonymous read access.
+Click Save to save your changes.
