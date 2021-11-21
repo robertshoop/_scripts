fc448f14bd35d01b23c1ee844d2a27e0157ef4cf
Robert Shoop <robertshoop@gmail.com>
2021-11-21 14:33:45 -0800

    Adding notes

--- _bash/app_servers/jenkins-ci/jenkins_password_reset_script.sh
+++ _bash/app_servers/jenkins-ci/jenkins_password_reset_script.sh
@@ -1,10 +1,11 @@
-Click on People on the left-hand navigation menu.
-Click on the Admin.
-Delete the user account.
-Navigate to Jenkins / Manage Jenkins.
-Click on Configure Global Security
-Check the Enable Security check box
-Under Security Realm, select Jenkins’ own user database
-In the Authorization section, select Logged-in users can do anything.
-Unselect Allow anonymous read access.
-Click Save to save your changes.
+# Work on automating the following:
+# Click on People on the left-hand navigation menu.
+# Click on the Admin.
+# Delete the user account.
+# Navigate to Jenkins / Manage Jenkins.
+# Click on Configure Global Security
+# Check the Enable Security check box
+# Under Security Realm, select Jenkins’ own user database
+# In the Authorization section, select Logged-in users can do anything.
+# Unselect Allow anonymous read access.
+# Click Save to save your changes.
