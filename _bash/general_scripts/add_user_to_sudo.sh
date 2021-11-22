95e952d190c3a8d7d9a018e15d314b9ff9b105d0
Robert Shoop <robertshoop@gmail.com>
2021-11-21 21:11:27 -0800

    adding sudo shell script

---
+++ _bash/general_scripts/add_user_to_sudo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Tested on Debian 11 with latest updates as of 11-21-2021
+# Use this script to install the sudo package on a Debian 11 box and then grant a user sudo permissions:
+# Run as root (if this is the first time running it) or a user with root / sudo permissions.
+
+#
+echo "What user needs to have sudo rights on this machine?:"
+read sudoUserName
+
+# Install sudo package
+apt-get install sudo -y
+
+# Add the user to the sudo group
+/sbin/adduser $sudoUserName sudo
+echo
+echo "The user $sudoUserName will need to log out and then back into the server to have sudo permissions."
+echo "As a test the user (afer logging back in) can run sudo echo 'Hello, world!' (including single quotes) to test sudo permissions"
+echo
